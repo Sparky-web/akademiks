2319d219597ae7863b25da061d2c0b378f9acfc6
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-04-25 21:36:34 +0200

    fix

--- src/components/custom/schedule/components/day-picker.tsx
+++ src/components/custom/schedule/components/day-picker.tsx
@@ -46,7 +46,12 @@ export default function DayPicker(props: DayPickerProps) {
             >
               <span className="text-xs">{day}</span>
               <span className="mb-2 text-base font-semibold">
-                {date.getDate() + 1}
+                {/* {
+                  DateTime.fromJSDate(date).toLocaleParts({
+                    day: "numeric",
+                  }).values(
+                } */}
+                {DateTime.fromJSDate(date).minus({ days: 1 }).toFormat("dd")}
               </span>
               <DayLoadProgress day={foundDay} />
             </button>
