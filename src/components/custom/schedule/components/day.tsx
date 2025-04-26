676dd669a25144198b0a1753c58f319fccbf905b
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-04-26 15:53:47 +0300

    fix

--- src/components/custom/schedule/components/day.tsx
+++ src/components/custom/schedule/components/day.tsx
@@ -3,51 +3,50 @@ import Lesson from "./lesson";
 import useActiveLessonId from "../utils/use-active-lesson-id";
 
 interface DayProps {
-    day: Day,
-    type: 'student' | 'teacher'
+  day: Day;
+  type: "student" | "teacher";
 }
 
-import { type Lesson as LessonType } from "~/types/schedule"
+import { type Lesson as LessonType } from "~/types/schedule";
 
 export type Slot = {
-    isActive: boolean,
-    index: number,
-    start: Date,
-    end: Date,
-    lessonsInSlot: LessonType[]
-}
+  isActive: boolean;
+  index: number;
+  start: Date;
+  end: Date;
+  lessonsInSlot: LessonType[];
+};
 
 export default function Day(props: DayProps) {
-    const activeLessonId = useActiveLessonId(props.day.lessons)
+  const activeLessonId = useActiveLessonId(props.day.lessons);
 
-    const lessons: Slot[] = []
+  const lessons: Slot[] = [];
 
-    for (let lesson of props.day.lessons) {
-        const found = lessons.find(e => e.index === lesson.index)
+  for (let lesson of props.day.lessons) {
+    const found = lessons.find((e) => e.index === lesson.index);
 
-        if (found) {
-            if (activeLessonId === lesson.id) {
-                found.isActive = true
-            }
-            found.lessonsInSlot.push(lesson)
-        }
-
-        else {
-            lessons.push({
-                isActive: activeLessonId === lesson.id,
-                index: lesson.index,
-                start: lesson.start,
-                end: lesson.end,
-                lessonsInSlot: [lesson]
-            })
-        }
+    if (found) {
+      if (activeLessonId === lesson.id) {
+        found.isActive = true;
+      }
+      found.lessonsInSlot.push(lesson);
+    } else {
+      lessons.push({
+        isActive: activeLessonId === lesson.id,
+        index: lesson.index,
+        start: lesson.start,
+        end: lesson.end,
+        lessonsInSlot: [lesson],
+      });
     }
+  }
 
-    return (
-        <div className="grid gap-4">
-            {!!props.day.lessons.length && lessons.map((lesson, i) => (
-                <Lesson key={i} slot={lesson} type={props.type} />
-            ))}
-        </div>
-    )
+  return (
+    <div className="grid gap-4">
+      {!!props.day.lessons.length &&
+        lessons.map((lesson, i) => (
+          <Lesson key={i} slot={lesson} type={props.type} />
+        ))}
+    </div>
+  );
 }
