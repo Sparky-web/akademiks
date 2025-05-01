931730fd33364c8e89b83dd8f658fa327cb2b13b
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-05-01 16:32:59 +0300

    feat(error): add global error boundary

--- src/components/custom/errors/initialization-error-card.tsx
+++ src/components/custom/errors/initialization-error-card.tsx
@@ -1,20 +1,42 @@
 // import Card from "../card";
 
 import { AlertCircle } from "lucide-react";
+import { Button } from "~/components/ui/button";
 import { H4, P } from "~/components/ui/typography";
 
 export default function InitializationErrorCard({
   message,
+  onReset,
 }: {
   message: string;
+  onReset?: () => void;
 }) {
   return (
-    <div className="grid gap-2 rounded-lg bg-red-100 p-4">
-      <H4 className="flex gap-2 text-base">
+    <div className="grid content-center justify-center justify-items-center gap-3 text-center">
+      <H4 className="flex justify-center gap-2 text-lg">
         <AlertCircle className="mt-1 h-5 w-5" />
         Что-то пошло не так
       </H4>
-      <P className="text-sm">{message}</P>
+      <P className="text-base">
+        Если ошибка повторяется, пожалуйста, сообщите об этом в Telegram -{" "}
+        <a
+          className="text-primary underline"
+          target="_blank"
+          href={"https://t.me/vladislavbabinov"}
+        >
+          @vladislavbabinov
+        </a>
+      </P>
+      <P className="text-muted-foreground text-base">{message}</P>
+      {onReset && (
+        <Button
+          variant="tenary"
+          onClick={onReset}
+          className="mt-2 max-w-[300px]"
+        >
+          Перезагрузить
+        </Button>
+      )}
     </div>
   );
 }
